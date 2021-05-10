f353db23575a28e931276495d0fa6be5f9ca2d5c
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-10 11:14:32 +0200

    Update template-home.php

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -13,15 +13,11 @@ defined( 'ABSPATH' ) || exit;
 get_header();
 ?>
 <div class="home-bg"></div>
-
 <div class="sponsors-container">
 <div class="sponsor">Huvudsponsor</div>
 <div class="sponsor">Huvudsponsor</div>
 <div class="sponsor">Huvudsponsor</div>
 <div class="sponsor">Huvudsponsor</div>
 </div>
-
-
-
 <?php
 get_footer();
