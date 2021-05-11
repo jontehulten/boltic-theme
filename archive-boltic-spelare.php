67a8a01f3d929caba57f33f8853ae679bf5a5734
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-11 03:04:02 +0200

    spelarsidan

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -49,8 +49,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 							
 							
 							the_title(
-								sprintf( '<h2 class="entry-title spelar-namn"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-								'</a></h2>'
+								sprintf( '<h3 class="entry-title spelar-namn"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+								'</a></h3>'
 							);
 							echo '</div>';	
 							
