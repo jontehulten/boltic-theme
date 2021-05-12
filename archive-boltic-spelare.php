1827b79a77d8009d5c227429efe8b99e66cab82f
Jonathan <78376822+jontehulten@users.noreply.github.com>
2021-05-12 15:28:33 +0200

    clickable pic test 4

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -50,7 +50,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 							
 							
 							the_title(
-								sprintf( '<span class="entry-title spelar-namn"></a></span>');
+								echo'<span class="entry-title spelar-namn"></a></span>';
 							echo '</div>';	
 							
 						}
