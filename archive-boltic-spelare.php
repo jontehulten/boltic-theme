81c0677156152b422b77462e79f7352ffa5ad7d9
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-13 01:43:49 +0200

    Update archive-boltic-spelare.php

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -49,7 +49,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 							
 							
 							the_title(
-								sprintf( '<span class="entry-title spelar-namn"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+								sprintf( '<span class="entry-title spelar-namn"><div class="col-3 text-center mb-4"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 								'</a></span>'
 							);
 							echo '</div>';	
