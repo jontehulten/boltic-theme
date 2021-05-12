fad1faeadc2597e8836465f2533d8a327405e875
Jonathan <78376822+jontehulten@users.noreply.github.com>
2021-05-12 15:23:18 +0200

    clickable pic test 2

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -45,7 +45,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							*/
 							echo '<div class="col-3 text-center mb-4">';
-							echo '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) );
+							echo '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ;
 							get_template_part( 'loop-templates/content', 'boltic-spelare' );
 							
 							
