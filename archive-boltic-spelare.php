2daf792b03f25f1f49e85d256de78467f514080e
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-13 04:11:23 +0200

    Update archive-boltic-spelare.php

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -44,11 +44,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 							* If you want to override this in a child theme, then include a file
 							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							*/
-							echo '<div class="col-3 text-center mb-4">';
-							//get_template_part( 'loop-templates/content', 'boltic-spelare' );
-							
 							
 							the_title(
+
+								echo '<div class="col-3 text-center mb-4">';
+								get_template_part( 'loop-templates/content', 'boltic-spelare' );
 								sprintf( '<span class="entry-title spelar-namn"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 								'</a></span>'
 							);
