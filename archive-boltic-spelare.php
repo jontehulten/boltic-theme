c437bd6cf5aa0c43793367d7bdda6e3e29e5a25b
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-13 02:49:46 +0200

    Update archive-boltic-spelare.php

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -46,11 +46,10 @@ $container = get_theme_mod( 'understrap_container_type' );
 							*/
 							echo '<div class="col-3 text-center mb-4">';
 							
-							the_title(
+							the_title( 							);
+								get_template_part( 'loop-templates/content', 'boltic-spelare' ), 
 								sprintf( '<span class="entry-title spelar-namn"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 								'</a></span>'
-							);
-							get_template_part( 'loop-templates/content', 'boltic-spelare' );
 							echo '</div>';	
 							
 						}
