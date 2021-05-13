7cbcdaf18605eb54576b2ce328c169f5339746f3
Jonathan <78376822+jontehulten@users.noreply.github.com>
2021-05-13 13:17:55 +0200

    dots false

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="sponsors-container">
 <?php //get_template_part( 'sidebar-templates/sidebar-hero' ); 
-      echo do_shortcode('[logoshowcase max_height="150" orderby="rand" logoshowcase dots=”false” extra_class="sponsors"]'); ?>
+      echo do_shortcode('[logoshowcase max_height="150" orderby="rand" dots=”false” extra_class="sponsors"]'); ?>
 </div>
 
 <div class="wrapper border-top" id="wrapper-footer">
