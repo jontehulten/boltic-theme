1d6d782d035563a26b78d7e021f3c1b4b2509456
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-13 02:18:13 +0200

    Update archive-boltic-spelare.php

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -45,11 +45,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
 							*/
 							echo '<div class="col-3 text-center mb-4">';
-							//get_template_part( 'loop-templates/content', 'boltic-spelare' );
+							$testing = get_template_part( 'loop-templates/content', 'boltic-spelare' );
 							
 							
 							the_title(
-								sprintf( '<span class="entry-title spelar-namn"><a href="%s" rel="bookmark"> ', esc_url( get_template_part( 'loop-templates/content', 'boltic-spelare' ) ) ),
+								sprintf( '<span class="entry-title spelar-namn"><a href="%s" rel="bookmark"> ', esc_url( $testing ) ),
 								'</a></span>'
 							);
 							echo '</div>';	
