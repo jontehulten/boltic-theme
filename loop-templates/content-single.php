de1ce555aed5f853837a02a28da4883588caea26
JosefineJonsson <josejons103@gmail.com>
2021-05-14 10:07:10 +0200

    Enskild nyhet

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -10,25 +10,22 @@ defined( 'ABSPATH' ) || exit;
 ?>
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-	<header class="entry-header">
+	<div class="single-nyhet">
+	<header class="entry-header ">
 
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-		<div class="entry-meta">
-
-			<?php understrap_posted_on(); ?>
-
-		</div><!-- .entry-meta -->
+		<span class="entry-date">Publicerat: <?php echo get_the_date(); ?></span>
 
 	</header><!-- .entry-header -->
-
+	<div class="single-nyhet-bild">
 	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	</div>
+	
 
 	<div class="entry-content">
-
+	
 		<?php the_content(); ?>
-
+		
 		<?php
 		wp_link_pages(
 			array(
@@ -40,10 +37,5 @@ defined( 'ABSPATH' ) || exit;
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
-
+	</div>
 </article><!-- #post-## -->
