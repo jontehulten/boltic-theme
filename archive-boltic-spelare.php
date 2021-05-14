9b6e8c1eeb590f3d3c1048aeb96d4a5fa71990e7
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-14 02:10:49 +0200

    Update archive-boltic-spelare.php

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -29,7 +29,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php
 				if ( have_posts() ) {
 					?>
-					<div class="hero-spelarsidan">hej allihopa</div>
+					<img class="hero-bild-spelarsidan" src="../img/hero-spelare.jpg" alt="Gruppbild alla spelare"></img>
 					<header class="page-header">
                     <h1>A-laget</h1>
 					</header><!-- .page-header -->
