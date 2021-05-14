97af66167f25d917a0a28793deb50a196a4e225a
Jonathan <78376822+jontehulten@users.noreply.github.com>
2021-05-14 15:17:21 +0200

    cover new try

--- archive-boltic-spelare.php
+++ archive-boltic-spelare.php
@@ -15,13 +15,13 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
+<img class="hero-bild-spelarsidan" src="../img/boltic-hero-spelare.jpeg" alt="Gruppbild alla spelare"></img>
+
 <div class="wrapper" id="archive-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
 
 		<div class="row" id="alla-spelare">
-		<img class="hero-bild-spelarsidan" src="../img/boltic-hero-spelare.jpeg" alt="Gruppbild alla spelare"></img>
-
 
 			<!-- Do the left sidebar check -->
 			<?php get_template_part( 'global-templates/left-sidebar-check' ); ?>
