56d40c3b3723e36e4d01772bdd81665c11db2c93
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-15 19:17:14 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <?php get_template_part( 'sidebar-templates/sidebar', 'footerfull' ); ?>
 
 <div class="sponsors-container">
+	<h1>Våra sponsorer</h1>
 <?php //get_template_part( 'sidebar-templates/sidebar-hero' ); 
       echo do_shortcode('[logoshowcase max_height="150" orderby="rand" dots="false" arrows="true" extra_class="sponsors"]'); ?>
 </div>
