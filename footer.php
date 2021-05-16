e5b3467da9fc504ca298b952bd56b178ff5c19ad
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-17 00:18:04 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -31,30 +31,30 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="col-md-12">
 
 				<footer class="site-footer" id="colophon">					
-					    <div id="footer-sidebar" class="row align-items-center text-center">
-						  <div id="footer-one" class="widget col-sm-4">
-							  <?php
-								  if(is_active_sidebar('footer-one')){
-								  	dynamic_sidebar('footer-one');
-								  }
-								  ?>
-						  </div>
-						  <div id="footer-two" class="widget col-sm-4">
-							  <?php
-								  if(is_active_sidebar('footer-two')){
-									dynamic_sidebar('footer-two');
-								  }
-							  ?>
-						  </div>
+					<div id="footer-sidebar" class="row align-items-center text-center">
+						<div id="footer-one" class="widget col-sm-4">
+							<?php
+								if(is_active_sidebar('footer-one')){
+								dynamic_sidebar('footer-one');
+								}
+								?>
+						</div>
+						<div id="footer-two" class="widget col-sm-4">
+							<?php
+								if(is_active_sidebar('footer-two')){
+								dynamic_sidebar('footer-two');
+								}
+							?>
+						</div>
 						<div id="footer-three" class="widget col-sm-4">
-							  <?php
-								  if(is_active_sidebar('footer-three')){
-									dynamic_sidebar('footer-three');
-								  }
-							  ?>
-						  </div>
-						</div>						
-				</div>				  	
+							<?php
+								if(is_active_sidebar('footer-three')){
+								dynamic_sidebar('footer-three');
+								}
+							?>
+						</div>
+					</div>						
+
 					<div class="site-info mt-5 text-center">
 
 						<p>@Copyright Boltic grupp 9<p>
