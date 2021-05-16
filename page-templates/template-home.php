c42ff79bd1519b3607796dcdf59adaf105d6cc35
aleksandarmlinar <60160624+aleksandarmlinar@users.noreply.github.com>
2021-05-17 01:49:35 +0200

    Update template-home.php

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -14,7 +14,7 @@ get_header();
 ?>
 
 <div class="home-bg d-flex align-items-end text-center pb-4">     
-    <div>
+    <div class="row">
         <div class="text-center">Senaste resultat</div>
     </div>
     <div class="row">
