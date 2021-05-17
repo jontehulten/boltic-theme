a95df9377b7869cf980b47cd36b2af6621104132
JosefineJonsson <josejons103@gmail.com>
2021-05-17 14:18:37 +0200

    Update category.php
    Ändrat titel

--- category.php
+++ category.php
@@ -31,7 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 					?>
 					<header class="page-header">
 						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						echo '<h1 class="page-title">' . single_cat_title( '', false ) . '</h1>';
                         echo '<p>Ska försöka ta bort Category: </p>';
 						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
