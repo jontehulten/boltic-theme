46a61c9d2c6a9c1a40a49dcd3c3aaffd7da30fb5
JosefineJonsson <josejons103@gmail.com>
2021-05-18 16:57:32 +0200

    Create template-vardegrund.php

---
+++ page-templates/template-vardegrund.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Template Name: Vardegrund Page
+ *
+ * Template for displaying a page without sidebar even if a sidebar widget is published.
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+
+?>
+
+<div class="wrapper" id="basic-page-wrapper">
+
+	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+
+		<div class="row">
+
+			<div class="col-md-12 content-area color-vardegrund" id="primary">
+
+				<main class="site-main" id="main" role="main">
+
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'page' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					}
+					?>
+
+				</main><!-- #main -->
+
+			</div><!-- #primary -->
+
+		</div><!-- .row end -->
+
+	</div><!-- #content -->
+    
+
+
+</div><!-- #full-width-page-wrapper -->
+
+<?php
+get_footer();
