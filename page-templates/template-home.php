5cb2eb107cf6cb2f68b8fed274799bba24fb3f97
Jonathan <78376822+jontehulten@users.noreply.github.com>
2021-05-20 21:24:09 +0200

    :))

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -14,11 +14,11 @@ get_header();
 ?>
 
 <div class="home-bg row align-items-end d-flex justify-content-around text-center pb-4">         
-    <div class="col-md-4 matcher-front" id="senaste-resultat">   
+<a href="#"><div class="col-md-4 matcher-front" id="senaste-resultat">   
         <div class="row justify-content-center pt-1">
                 <div> <h4>Senaste resultat</h4> </div>
         </div>
-        <a href="#" class="clickBox"><div class="row align-items-center">
+            <div class="row align-items-center">
                         <div class="col text-nowrap">
                             <img src="http://toto.mkv.kau.se/vt21/wd2g_09/wp-content/uploads/2021/05/if-boltic-logotyp-g9-e1621032946984.png" clas="logo-resultat" alt="If Boltic logotyp">
                             <p>If Boltic</p>
@@ -30,12 +30,12 @@ get_header();
                         </div>
         </div></a>
     </div> 
-    <div class="col-md-4 matcher-front" id="nasta-match">   
+    <a href="#"><div class="col-md-4 matcher-front" id="nasta-match">   
     <div class="row justify-content-center pt-1">
                 <div> <h4>Nästa match</h4> </div>
                 <p class="date">Lördag 22/05 kl 19:00</p>
         </div>
-        <a href="#" class="clickBox"><div class="row newResult">
+            <div class="row newResult">
                         <div class="col text-nowrap">
                             <img src="http://toto.mkv.kau.se/vt21/wd2g_09/wp-content/uploads/2021/05/if-boltic-logotyp-g9-e1621032946984.png" class="logo-resultat" alt="If Boltic logotyp">
                             <p>If Boltic</p>
@@ -45,8 +45,8 @@ get_header();
                             <img src="http://toto.mkv.kau.se/vt21/wd2g_09/wp-content/uploads/2021/05/hammarby_if-logo-1.png" class="logo-resultat" alt="Hammarby logotyp">
                             <p>Hammarby</p>
                         </div>
-        </div></a>
-    </div> 
+        </div>
+    </div></a> 
 </div>
 
 <!--<img class="home-bg" src=" <?php //echo get_template_directory_uri(); ?>/img/Boltic-LAIK11.jpg"></img> -->
